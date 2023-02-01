302a5d7c1f1ea2aca06ad518b60cc5e084a39f31
raikbitters <raikbitters@gmail.com>
2023-02-01 17:30:11 +0400

    Add node package for gen code

---
+++ index.js
@@ -0,0 +1,7 @@
+const reportingMessages = require("./reportportal/reporting/v1/reporting_pb.js");
+const reportingServices = require ("./reportportal/reporting/v1/reporting_grpc_pb.js");
+
+module.exports = {
+  reportingMessages,
+  reportingServices
+};
