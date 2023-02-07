b784e907347a146f1810ccf729204bf3bb15671c
raikbitters <raikbitters@gmail.com>
2023-02-07 18:12:09 +0400

    Add Tonic as a builder over Prost

--- src/build.rs
+++ src/build.rs
@@ -1,6 +1,9 @@
-use std::io::Result;
-
-fn main() -> Result<()> {
-    prost_build::compile_protos(&["api/proto/reportportal/reporting/v1/reporting.proto"], &["api/proto/"])?;
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    tonic_build::configure()
+    .compile(
+        &["api/proto/reportportal/reporting/v1/reporting.proto"],
+        &["api/proto/"],
+    )?;
+    
     Ok(())
 }
