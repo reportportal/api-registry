869eb4a4394039bff0c91f87972e281e3e801c77
raikbitters <raikbitters@gmail.com>
2023-02-08 19:37:46 +0400

    Delete env BUILD_PATH

--- src/build.rs
+++ src/build.rs
@@ -1,16 +1,10 @@
-use std::env;
-use std::fs;
-
 fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     // Because docs.rs uses an old version protobuf compiler that doesn't support
     // option fields for Proto3, we avoid executing the build script on docs.rs.
     if std::env::var("DOCS_RS").is_err() {
-        let path = concat!("src/", env!("BUILD_DIR"));
-        fs::create_dir_all(path)?;
-
         tonic_build::configure()
-        .out_dir(path)
+        .out_dir("src/build/proto/rust/")
         .compile(
             &["api/proto/reportportal/reporting/v1/reporting.proto"],
             &["api/proto/"],
--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,13 @@
 pub mod reportportal_grpc {
     pub mod reporting {
         pub mod v1 {
-            include!(concat!(env!("BUILD_DIR"), "/reportportal.reporting.v1.rs"));
+            include!("build/proto/rust/reportportal.reporting.v1.rs");
         }
     }
 }
 
 pub mod google {
     pub mod r#type {
-        include!(concat!(env!("BUILD_DIR"), "/google.r#type.rs"));
+        include!("build/proto/rust/google.r#type.rs");
     }
 }
