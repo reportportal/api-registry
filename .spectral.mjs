eacf0a21b08e0ee40990ebba1ec495f213822c64
PeeAyBee <rrrizon@gmail.com>
2023-09-04 22:32:35 +0300

    Added creating a global integration for gitlab plugin

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjIxNTE3Ng/spectral.js?branch_slug=main&token=dce926f9-2f76-49c4-8262-878dd48c438b";
+export default { extends: ruleset };
